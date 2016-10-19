8dec82f9ea1a111703b493340afd7e00e1031868
bulka4927 <bulka4927@HRH-IC0027338.wrdsb.ca>
2016-10-19 09:27:08 -0400

    A4Q4Finished

--- src/A4Q4.java
+++ src/A4Q4.java
@@ -15,28 +15,34 @@ public class A4Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Scanner in = new Scanner(System.in);
-       
-        //Ask howm much each thing costs
-        System.out.println("How much does the food for prom cost? ");
-        
-        System.out.println("How much does the DJ cost? ");
-        
-        System.out.println("How much does it cost ot rent the hall? ");
-        
-        System.out.println("how much does decorations cost? ");
-        
-        System.out.println("How much does it cost for staff? ");
-        
-        System.out.println("How much for miscellaneous costs? ");
-        
-        System.out.println("The total cost is $");
-        
-        
-        //Total it all up
-        double amount = in.nextDouble();
+        Scanner in = new Scanner(System.in);
 
-        double tickets = amount/35;
-        
+        //Ask how much each thing costs
+        System.out.println("How much does the food for prom cost? ");
+        double number1 = in.nextDouble();
+
+        System.out.println("How much does the DJ cost? ");
+        double number2 = in.nextDouble();
+
+        System.out.println("How much does it cost ot rent the hall? ");
+        double number3 = in.nextDouble();
+
+        System.out.println("How much does decorations cost? ");
+        double number4 = in.nextDouble();
+
+        System.out.println("How much does it cost for staff? ");
+        double number5 = in.nextDouble();
+
+        System.out.println("How much for miscellaneous costs? ");
+        double number6 = in.nextDouble();
+
+        //Total it all up
+        double amount = number1 + number2 + number3 + number4 + number5 + number6;
+        System.out.println("The total cost is $" + amount);
+
+
+        double tickets = Math.ceil(amount / 35);
+        System.out.println(" You will need to sell " + tickets + " tickets to break even.");
+
     }
 }
